543d57d299506223fbba7ca201768ace8f401e6f
geun99 <lich1115@naver.com>
2024-06-01 01:04:44 +0900

    [feat] vercel.json추가, 스크롤탑 기능 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalStyle } from "./style/global.ts";
+import ScroolToTop from "./hooks/ScroolToTop.tsx";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <GlobalStyle />
+        <ScroolToTop />
         <App />
       </QueryClientProvider>
     </BrowserRouter>
